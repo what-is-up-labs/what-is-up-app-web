a5363307343bdabff9f3d674aa8fd834dd08800c
root <root@whatisupapp.com>
2016-06-23 02:00:54 -0400

    signup stuff

--- js/script.js
+++ js/script.js
@@ -4,18 +4,18 @@ function subscribe() {
 
 	//alert (dataString);return false;  
 
-	$.ajax({  
-	  type: "POST",  
-	  url: "subscribe.php",  
-	  data: dataString,  
-	  success: function(data) {  
-	  	if (data == "success") {
+//	$.ajax({  
+//	  type: "POST",  
+//	  url: "subscribe.php",  
+//	  data: dataString,  
+//	  success: function(data) {  
+//	  	if (data == "success") {
 		    $('#signuparea').hide();  
 		    $('#thankyoumsg').fadeIn(1000);  
-	    } else {
-	    	alert("There was an error processing your request.  Please try again later.\n\n" + data);
-	    }
-	  }  
-	});  
+//	    } else {
+//	    	alert("There was an error processing your request.  Please try again later.\n\n" + data);
+//	    }
+//	  }  
+//	});  
 	return false;  
 }
